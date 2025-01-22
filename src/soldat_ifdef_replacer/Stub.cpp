697c9961ca2fc0b0ad12add3bfaba36620dbda8e
Dariusz Kluska <darkenk@gmail.com>
2025-01-22 21:13:47 +0100

    Further modifications to soldat_ifdef_replacer

--- src/soldat_ifdef_replacer/Stub.cpp
+++ src/soldat_ifdef_replacer/Stub.cpp
@@ -1,7 +1,98 @@
+#define SERVER
+
+#ifdef SERVER
+[[maybe_unused]] int global = 0;
+#endif // SERVER
+
+namespace Config
+{
+
+enum Module
+{
+  CLIENT_MODULE,
+  SERVER_MODULE,
+  TEST_MODULE,
+  INVALID_MODULE
+};
+
+namespace defaults
+{
+
+constexpr Module GetModule() noexcept
+{
+  return INVALID_MODULE;
+}
+
+} // namespace defaults
+
+using namespace defaults;
+
+} // namespace Config
+
+template <Config::Module M>
+class TestClass
+{
+  void testfunction();
+};
+
+template <Config::Module M>
+void TestClass<M>::testfunction()
+{
+  bool t = true;
+  #ifdef SERVER
+  if (t != true)
+  #endif // SERVER
+  {
+  int nice = 0;
+  }
+}
+
+
+#ifdef SERVER
+void testfunction_server()
+{
+    int nice = 0;
+}
+#endif // SERVER
+
+template <typename M>
+void templated_function()
+{
+    #ifdef SERVER
+    int nice = 0;
+    #endif // SERVER
+}
+
+template <Config::Module M>
+void templated_function2()
+{
+    #ifdef SERVER
+    int nice = 0;
+    #endif // SERVER
+}
+
 
 void testfunction()
 {
     #ifdef SERVER
     int nice = 0;
+    #endif // SERVER
+
+    #ifdef SERVER
+    int s_nice = 0;
+    #else
+    int s_bad = 0;
+    #endif // SERVER
+
+    #ifdef CLIENT
+    int c_nice = 0;
+    #else
+    int c_bad = 0;
+    #endif // CLIENT
+
+    #ifdef SERVER
+    #if 0
+    int s_nice = 0;
+    #endif // 0
     #endif // SERVER
 }
--- src/soldat_ifdef_replacer/soldat_ifdef_replacer.cpp
+++ src/soldat_ifdef_replacer/soldat_ifdef_replacer.cpp
@@ -1,126 +1,241 @@
-#include "clang/AST/ASTContext.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang-tidy/ClangTidy.h"
 #include "clang-tidy/ClangTidyCheck.h"
 #include "clang-tidy/ClangTidyModule.h"
 #include "clang-tidy/ClangTidyModuleRegistry.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/TypeLoc.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Lex/PPCallbacks.h"
 #include "clang/Lex/Preprocessor.h"
-#include "clang/Frontend/CompilerInstance.h"
 
 using namespace clang;
 using namespace clang::tidy;
 using namespace clang::ast_matchers;
 
-class SoldatIfdefReplacer : public ClangTidyCheck
+struct IfdefLocation
 {
-public:
-    SoldatIfdefReplacer(StringRef Name, ClangTidyContext* Context) : ClangTidyCheck(Name, Context)
-    {
-    }
-    void registerPPCallbacks(const SourceManager &SM, Preprocessor *PP, Preprocessor *ModuleExpanderPP) override;
+  SourceLocation Start;
+  SourceLocation Else;
+  SourceLocation End;
+  SourceLocation MacroEnd;
+  bool isIfndef;
 };
+
+struct IfdefPasser
+{
+  std::vector<IfdefLocation> &IfdefLocations;
+};
+
+namespace
+{
+// gets the location of the # character in a #define or #ifdef directive
+SourceLocation GetHashLoc(SourceLocation Loc) { return Loc.getLocWithOffset(-1); }
+
+AST_MATCHER_P(FunctionDecl, hasIfdefs, IfdefPasser, Locations)
+{
+
+  SourceRange Range = Node.getSourceRange();
+
+  auto inRange = std::ranges::any_of(Locations.IfdefLocations, [&](const auto &Ifdef) {
+    return Range.fullyContains(SourceRange(Ifdef.Start, Ifdef.End));
+  });
+
+  static constexpr auto debug = false;
+  if constexpr (debug)
+  {
+    const SourceManager &SM = Finder->getASTContext().getSourceManager();
+    llvm::errs() << "FunctionDecl SourceRange: " << Range.getBegin().printToString(SM) << " - "
+                 << Range.getEnd().printToString(SM) << " hasIfdefs "
+                 << (inRange ? "SUCCESS" : "FAIL") << "\n";
+  }
+  return inRange;
+}
+} // namespace
 
 class IfdefPPCallbacks : public PPCallbacks
 {
 public:
-    IfdefPPCallbacks(ClangTidyCheck &Check, const SourceManager &SM) : Check(Check), SM(SM) {}
+  IfdefPPCallbacks(ClangTidyCheck &Check, const SourceManager &SM,
+                   std::vector<IfdefLocation> &IfdefLocations)
+    : Check(Check), SM(SM), IfdefLocations(IfdefLocations)
+  {
+  }
 
-    void Ifdef(SourceLocation Loc, const Token &MacroNameTok, const MacroDefinition &MD) override {
-        std::string MacroName = MacroNameTok.getIdentifierInfo()->getName().str();
-        if (MacroName == "SERVER") {
-            DirectiveStack.push_back({Loc, true, false}); // isServer=true, hasElse=false
-            std::string ReplacementText = "if constexpr (Config::IsServer(M)) {";
-            Check.diag(Loc, "replacing #ifdef with if constexpr")
-                << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Loc), MacroNameTok.getEndLoc()), ReplacementText);
-        } else {
-            DirectiveStack.push_back({Loc, false, false});
-        }
-    }
+  void If(SourceLocation Loc, SourceRange ConditionRange,
+          ConditionValueKind ConditionValue) override
+  {
+    DirectiveStack.emplace_back();
+  }
 
-    void Ifndef(SourceLocation Loc, const Token &MacroNameTok, const MacroDefinition &MD) override
+  void Ifdef(SourceLocation Loc, const Token &MacroNameTok, const MacroDefinition &MD) override
+  {
+    std::string MacroName = MacroNameTok.getIdentifierInfo()->getName().str();
+    DirectiveStack.emplace_back();
+    if (MacroName == "SERVER")
     {
-        std::string MacroName = MacroNameTok.getIdentifierInfo()->getName().str();
-        if (MacroName == "SERVER") {
-            DirectiveStack.push_back({Loc, true, false}); // isServer=true, hasElse=false
-            std::string ReplacementText = "if constexpr (Config::IsServer(M)) {";
-            Check.diag(Loc, "replacing #ifdef with if constexpr")
-                << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Loc), MacroNameTok.getEndLoc()), ReplacementText);
-        } else {
-            DirectiveStack.push_back({Loc, false, false});
-        }
+      DirectiveStack.back().Loc = Loc;
+      DirectiveStack.back().MacroEndLoc = MacroNameTok.getEndLoc();
     }
+  }
 
-    void Else(SourceLocation Loc, SourceLocation IfLoc) override {
-        if (!DirectiveStack.empty()) {
-            auto &current = DirectiveStack.back();
-            if (current.isServer && !current.hasElse) {
-                current.hasElse = true;
-                Check.diag(Loc, "replacing #else with } else {")
-                    << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Loc), Loc), "} else {");
-            }
-        }
-    }
-
-    void Endif(SourceLocation Loc, SourceLocation IfLoc) override
+  void Ifndef(SourceLocation Loc, const Token &MacroNameTok, const MacroDefinition &MD) override
+  {
+    std::string MacroName = MacroNameTok.getIdentifierInfo()->getName().str();
+    DirectiveStack.emplace_back();
+    if (MacroName == "SERVER")
     {
-        if (!DirectiveStack.empty()) {
-            if (DirectiveStack.back().isServer) { // if it's a SERVER macro
-                // // Find start of #endif directive
-                // SourceLocation HashLoc = Loc.getLocWithOffset(-1);
-                
-                // // Get the full extent of #endif directive
-                // Token EndifTok;
-                // Lexer::getRawToken(Loc, EndifTok, SM, LangOptions());
-                // SourceLocation EndifEnd = EndifTok.getEndLoc();
-                Check.diag(Loc, "replacing #endif with closing brace") 
-                    << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Loc), Loc), "}");
-            }
-            DirectiveStack.pop_back();
-        }
+      DirectiveStack.back().Loc = Loc;
+      DirectiveStack.back().MacroEndLoc = MacroNameTok.getEndLoc();
+      DirectiveStack.back().isIfndef = true;
     }
+  }
+
+  void Else(SourceLocation Loc, SourceLocation IfLoc) override
+  {
+    auto &current = DirectiveStack.back();
+    if (current.Loc.isValid() && !current.ElseLoc.isValid())
+    {
+      current.ElseLoc = Loc;
+    }
+  }
+
+  void Endif(SourceLocation Loc, SourceLocation IfLoc) override
+  {
+    if (DirectiveStack.back().Loc.isValid())
+    { // if it's a SERVER macro
+      IfdefLocations.push_back({IfLoc, DirectiveStack.back().ElseLoc, Loc,
+                                DirectiveStack.back().MacroEndLoc, DirectiveStack.back().isIfndef});
+    }
+    DirectiveStack.pop_back();
+  }
 
 private:
-    SourceLocation GetHashLoc(SourceLocation Loc) {
-        return Loc.getLocWithOffset(-1);
-    }
-
-    struct DirectiveInfo {
-        SourceLocation Loc;
-        bool isServer;
-        bool hasElse;
-    };
-    ClangTidyCheck &Check;
-    [[maybe_unused]] const SourceManager &SM;
-    std::vector<DirectiveInfo> DirectiveStack; // Tracks ifdef/ifndef locations and if they're SERVER
+  struct DirectiveInfo
+  {
+    SourceLocation Loc;
+    SourceLocation ElseLoc;
+    SourceLocation MacroEndLoc;
+    bool isIfndef = false;
+  };
+  [[maybe_unused]] ClangTidyCheck &Check;
+  [[maybe_unused]] const SourceManager &SM;
+  std::vector<DirectiveInfo> DirectiveStack; // Tracks ifdef/ifndef locations and if they're SERVER
+  std::vector<IfdefLocation> &IfdefLocations;
 };
 
-
-void SoldatIfdefReplacer::registerPPCallbacks(const SourceManager &SM, Preprocessor *PP, Preprocessor *ModuleExpanderPP)
+class SoldatMatchCallback : public MatchFinder::MatchCallback
 {
-    PP->addPPCallbacks(std::make_unique<IfdefPPCallbacks>(*this, SM));
-}
+public:
+  explicit SoldatMatchCallback(ClangTidyCheck &Check, std::vector<IfdefLocation> &IfdefLocations)
+    : Check(Check), IfdefLocations(IfdefLocations)
+  {
+  }
 
-namespace {
+  void run(const MatchFinder::MatchResult &Result) override
+  {
+    if (const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("function_with_ifdef"))
+    {
+      MatchedDecl->dump();
+      bool isTemplate =
+        MatchedDecl->getDescribedTemplate() != nullptr ||
+        MatchedDecl->getTemplateSpecializationInfo() != nullptr ||
+        MatchedDecl->getMemberSpecializationInfo() != nullptr ||
+        ((dyn_cast<CXXRecordDecl>(MatchedDecl->getParent()) != nullptr) &&
+         dyn_cast<CXXRecordDecl>(MatchedDecl->getParent())->getDescribedClassTemplate() != nullptr);
+      if (!isTemplate)
+      {
+        std::string TemplatePrefix = "template<Config::Module M>\n";
+        Check.diag(MatchedDecl->getLocation(), "converting to template function")
+          << FixItHint::CreateInsertion(MatchedDecl->getBeginLoc(), TemplatePrefix);
+      }
+      for (const auto &Ifdef : IfdefLocations)
+      {
+        if (!MatchedDecl->getSourceRange().fullyContains(SourceRange(Ifdef.Start, Ifdef.End)))
+        {
+          continue;
+        }
+        if (Ifdef.isIfndef)
+        {
+          std::string ReplacementText = "if constexpr (!Config::IsServer(M)) {";
+          Check.diag(Ifdef.Start, "replacing #ifndef with if constexpr")
+            << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Ifdef.Start), Ifdef.MacroEnd),
+                                            ReplacementText);
+        }
+        else
+        {
+          std::string ReplacementText = "if constexpr (Config::IsServer(M)) {";
+          Check.diag(Ifdef.Start, "replacing #ifdef with if constexpr")
+            << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Ifdef.Start), Ifdef.MacroEnd),
+                                            ReplacementText);
+        }
+        if (Ifdef.Else.isValid())
+        {
+          Check.diag(Ifdef.Else, "replacing #else with } else {") << FixItHint::CreateReplacement(
+            SourceRange(GetHashLoc(Ifdef.Else), Ifdef.Else), "} else {");
+        }
+
+        Check.diag(Ifdef.End, "replacing #endif with closing brace")
+          << FixItHint::CreateReplacement(SourceRange(GetHashLoc(Ifdef.End), Ifdef.End), "}");
+      }
+    }
+  }
+
+private:
+  ClangTidyCheck &Check;
+  std::vector<IfdefLocation> &IfdefLocations;
+};
+
+class SoldatIfdefReplacer : public ClangTidyCheck
+{
+public:
+  SoldatIfdefReplacer(StringRef Name, ClangTidyContext *Context)
+    : ClangTidyCheck(Name, Context), Callback(*this, IfdefLocations)
+  {
+  }
+
+  void registerPPCallbacks(const SourceManager &SM, Preprocessor *PP,
+                           Preprocessor *ModuleExpanderPP) override
+  {
+    PP->addPPCallbacks(std::make_unique<IfdefPPCallbacks>(*this, SM, IfdefLocations));
+  }
+
+  void registerMatchers(MatchFinder *Finder) override
+  {
+    auto HasIfdefMatcher =
+      functionDecl(hasIfdefs(IfdefPasser{IfdefLocations})).bind("function_with_ifdef");
+
+    Finder->addMatcher(HasIfdefMatcher, &Callback);
+  }
+
+private:
+  SoldatMatchCallback Callback;
+  std::vector<IfdefLocation> IfdefLocations;
+};
+
+namespace
+{
 
 class SoldatIfdefReplacerModule : public ClangTidyModule
 {
 public:
-    void addCheckFactories(ClangTidyCheckFactories& CheckFactories) override
-    {
-        CheckFactories.registerCheck<SoldatIfdefReplacer>("soldat-ifdef-replacer");
-    }
+  void addCheckFactories(ClangTidyCheckFactories &CheckFactories) override
+  {
+    CheckFactories.registerCheck<SoldatIfdefReplacer>("soldat-ifdef-replacer");
+  }
 };
 
-}  // namespace
+} // namespace
 
-namespace clang::tidy {
+namespace clang::tidy
+{
 
 // Register the module using this statically initialized variable.
-static ClangTidyModuleRegistry::Add<::SoldatIfdefReplacerModule> soldatIfdefReplacerInit("soldat-ifdef-replacer-module",
-                                                                                       "Adds 'soldat-ifdef-replacer' checks.");
+static ClangTidyModuleRegistry::Add<::SoldatIfdefReplacerModule> soldatIfdefReplacerInit(
+  "soldat-ifdef-replacer-module", "Adds 'soldat-ifdef-replacer' checks.");
 
-// This anchor is used to force the linker to link in the generated object file and thus register the module.
+// This anchor is used to force the linker to link in the generated object file and thus register
+// the module.
 volatile int awesomePrefixCheckAnchorSource = 0;
 
-}  // namespace clang::tidy
+} // namespace clang::tidy
